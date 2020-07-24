1a8589b4b354426db3b28d63db1846f9bc3ebfc9
SeoSeunghun <201602012@o.cnu.ac.kr>
2020-07-24 21:56:53 +0900

    ReactNatice docs : JSX

--- Cat.js
+++ Cat.js
@@ -5,11 +5,14 @@ import { Text } from 'react-native';
 /* The rest of this introduction to React uses cats in its examples: 
  * friendly, approachable creatures that need names and a cafe to work in. 
  * Here is your very first Cat component:*/ 
-
  const Cat = () => {
+  const name = "Maru";
   return (
-    <Text>Hello, I am your cat!</Text>
+  <Text>Hello, I am {getFullName("Seo","Seunghun","")}</Text>
   );
 }
 
+const getFullName = (firstName, secondName, thirdName) => {
+    return firstName + " " + secondName + " " + thirdName;
+}
 export default Cat;
