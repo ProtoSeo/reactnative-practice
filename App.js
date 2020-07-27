3b8ce3adead7164c22f704bcfd24070f91d0af64
SeoSeunghun <201602012@o.cnu.ac.kr>
2020-07-28 00:17:03 +0900

    Combine all three navigators

--- App.js
+++ App.js
@@ -1,115 +1,98 @@
-import * as React from 'react';
-import { View, Text, Button ,StyleSheet} from 'react-native';
+import React from 'react';
+import { Button, View, Text } from 'react-native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { createStackNavigator} from '@react-navigation/stack';
 import { Ionicons } from '@expo/vector-icons';
-import { NavigationContainer, DrawerActions } from '@react-navigation/native';
-import {
-  createDrawerNavigator,
-  DrawerContentScrollView,
-  DrawerItemList,
-  DrawerItem,
-} from '@react-navigation/drawer';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-
 const Tab = createBottomTabNavigator();
-
-// Home화면 
-function HomeScreen({ navigation }) {
+function HomeScreen() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Home Screen</Text>
-      <Button
-        title="Open drawer"
-        onPress={() => navigation.dispatch(DrawerActions.openDrawer())}
-      />
-      <Button
-        title="Toggle drawer"
-        onPress={() => navigation.dispatch(DrawerActions.toggleDrawer())}
+    <Tab.Navigator
+    screenOptions={({ route }) => ({
+      tabBarIcon: ({ focused, color, size }) => {
+     let iconName;
+     if (route.name === 'TabA') {
+        iconName = focused
+        ? 'ios-information-circle'
+        : 'ios-information-circle-outline';
+      } else if (route.name === 'TabB') {
+        iconName = focused
+        ? 'ios-list-box'
+        : 'ios-list';
+      }
+return <Ionicons name={iconName} size={size} color={color}     />;
+        },
+      })}
+      tabBarOptions={{
+      activeTintColor: 'tomato',
+      inactiveTintColor: 'gray',
+      }}
+    >
+        <Tab.Screen name="TabA" component={TabAScreen} />
+        <Tab.Screen name="TabB" component={TabBScreen} />
+    </Tab.Navigator>
+  );
+}
+function NotificationsScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>No New Notifications!</Text>
+      <Button 
+      onPress={() => navigation.goBack()}
+      title="Go back home"
       />
     </View>
   );
 }
-// Notifications Screen 
-function Notifications() {
+const Stack = createStackNavigator();
+function TabAScreen() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Notifications Screen</Text>
+    <Stack.Navigator>
+      <Stack.Screen name="TabA Home" component={TabADetailsScreen} />
+      <Stack.Screen name="TabA Details" component={Details} />
+    </Stack.Navigator>
+  );
+}
+function TabADetailsScreen({navigation}) {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center',  alignItems: 'center' }}>
+      <Text>
+        Welcome to TabA page!
+      </Text>
+      <Button 
+      onPress={() => navigation.navigate('TabA Details')}
+      title="Go to TabA Details"
+      />
     </View>
   );
 }
-// 새로운 SettingScreen 
-function SettingsScreen() {
+function Details() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Settings!</Text>
+    <View style={{ flex: 1, justifyContent: 'center',  alignItems: 'center' }}>
+      <Text>
+        TabA Details here!
+      </Text>
     </View>
   );
 }
-
-// DrawerView에 무엇을 넣을것인지를 정함
-function CustomDrawerContent(props) {
+function TabBScreen() {
   return (
-    // props를 이용해서 무엇을 전달하고있는것인지???
-    <DrawerContentScrollView {...props}>
-      {/* // MyDrawer의 Content를 정하는것 props 는 MyDrawer에서 반환한 Screen을 가지고 있는다. */}
-      <DrawerItemList {...props} />
-      <DrawerItem
-        label="Close drawer"
-        onPress={() => props.navigation.dispatch(DrawerActions.closeDrawer())}
-      />
-      <DrawerItem
-        label="Toggle drawer"
-        onPress={() => props.navigation.dispatch(DrawerActions.toggleDrawer())}
-      />
-    </DrawerContentScrollView>
+    <View>
+      <Text style={{textAlign: 'center', marginTop: 300}}>
+        Welcome to TabB page!
+      </Text>
+    </View>
   );
 }
-
-// DrawerView를 생성하는 것
 const Drawer = createDrawerNavigator();
-
-// DrawerView를 생성하는 것
-function MyDrawer() {
-  return (
-    <Drawer.Navigator drawerContent={props => <CustomDrawerContent {...props} />}>
-      <Drawer.Screen name="Home" component={HomeScreen} />
-      <Drawer.Screen name="Notifications" component={Notifications} />
-      <Drawer.Screen name="Settings" component={SettingsScreen} />
-    </Drawer.Navigator>
-    
-  );
-}
-
 export default function App() {
   return (
     <NavigationContainer>
-      <MyDrawer />
-      <Tab.Navigator
-        screenOptions={({ route }) => ({
-          tabBarIcon: ({ focused, color, size }) => {
-            let iconName;
-
-            if (route.name === 'Home') {
-              iconName = focused ? 'ios-information-circle' : 'ios-information-circle-outline';
-            } else if (route.name === 'Settings') {
-              iconName = focused ? 'ios-list-box' : 'ios-list';
-            }
-
-            // You can return any component that you like here!
-            return <Ionicons name={iconName} size={size} color={color} />;
-          },
-        })}
-        tabBarOptions={{
-          activeTintColor: 'tomato',
-          inactiveTintColor: 'gray',
-        }}
-      >
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Settings" component={SettingsScreen} />
-      </Tab.Navigator>
+      <Drawer.Navigator initialRouteName="Home">
+        <Drawer.Screen name="Home" component={HomeScreen} />
+        <Drawer.Screen name="Notifications" component={NotificationsScreen} />
+      </Drawer.Navigator>
     </NavigationContainer>
-  );
+  )
 }
-
-const styles = StyleSheet.create({
-  
-})
